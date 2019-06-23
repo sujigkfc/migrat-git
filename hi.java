8c7dfc5da92afa9e4b1df1c2b27bc4ca527b52bd
root <root@localhost.localdomain>
2019-06-23 22:38:17 +0530

    commit hi file modified by linux

--- hi.java
+++ hi.java
@@ -2,6 +2,8 @@ class hi
 {
 public static void main(String[] args)
 {
-System.out.println("hi from hi");
+System.out.println("hi from hidev linux");
+{#devlinux
+System.out.println("hi from hii");#dev2
 }
 }
